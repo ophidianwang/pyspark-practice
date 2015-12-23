7d557e95ac448d2ba3d99027cc167c8450ae5a7c
ophidianwang <aa000017@gmail.com>
2015-12-23 09:54:28 +0800

    add script for transform amazon edge with weight

--- PIC_Amazon20030601.py
+++ PIC_Amazon20030601.py
@@ -13,34 +13,15 @@ import datetime
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.clustering import PowerIterationClustering, PowerIterationClusteringModel
 
+
+
+
+#start working with spark
 app_name = "PIC_Amazon_20030601"
 master = "spark_url"
 source_path = "D:\dataset\Amazon0601.txt"
-
 conf = SparkConf().setAppName(app_name).setMaster(master)
 sc = SparkContext(conf=conf)
-
-#load edge
-start_time = datetime.datetime.now();
-edge_list = []
-with open(source_path, "r") as source_file:
-    for n,line in enumerate( source_file.readlines() ):
-        if line[0] == "#":
-            continue
-        edge_proto = line.strip().split("\t")
-        edge_proto.append(1.0)
-        edge_list.append( tuple( edge_proto ) )
-        if n>10000:
-            break
-        
-"""
-should I weight vertex on how many edge does it have, and weight edges by the two vertex?
-maybe use the concept of pagerank? total node weight set to 1, give each edge weight (1/edge_num_of_node)
-If so:
-1. parse all edge record and accumulate vertex weight
-2. assign edge weight by vertex weight
-3. assign to networkx/spark link analysis model
-"""
 
 rdd = sc.parallelize(edge_list, 2)  #parallelize data set
 parse_done_time = datetime.datetime.now();
---
+++ transform_amazon.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 09:41:39 2015
+
+@author: USER
+"""
+
+import datetime
+
+
+#Load edge
+start_time = datetime.datetime.now();
+
+edge_list = []
+vertex_accumulate = {}
+edge_limit = 3000
+
+source_path = "D:\\dataset\\Amazon0601.txt"
+with open(source_path, "r") as source_file:
+    for n,line in enumerate( source_file.readlines() ):
+        if line[0] == "#":
+            continue
+        two_vertex = line.strip().split("\t")
+        if two_vertex[0] in vertex_accumulate:
+            vertex_accumulate[ two_vertex[0] ] += 1
+        else:
+            vertex_accumulate[ two_vertex[0] ] = 1
+        if two_vertex[1] in vertex_accumulate:
+            vertex_accumulate[ two_vertex[1] ] += 1
+        else:
+            vertex_accumulate[ two_vertex[1] ] = 1
+        edge_list.append( tuple( two_vertex ) )
+        #if n>edge_limit:
+            #break
+        
+"""
+should I weight vertex on how many edge does it have, and weight edges by the two vertex?
+maybe use the concept of pagerank? total node weight set to 1, give each edge weight (1/edge_num_of_node)
+If so:
+1. parse all edge record and accumulate vertex weight
+2. assign edge weight by vertex weight
+3. assign to networkx/spark link analysis model
+"""
+vertex_weight = {}
+for vertex_id in vertex_accumulate:
+    vertex_weight[ vertex_id ] = 1.0/vertex_accumulate[ vertex_id ]
+#print(vertex_accumulate)
+print(vertex_weight)
+
+edge_weight_list = []
+edge_weight_pool = []
+for pro,copro in edge_list:
+    edge_weight = (vertex_weight[pro] + vertex_weight[copro])
+    edge_weight_list.append( [pro, copro, edge_weight] )
+    edge_weight_pool.append( edge_weight )
+print(edge_weight_list)
+
+parse_done_time = datetime.datetime.now();
+print( "spend " + str( parse_done_time.timestamp() - start_time.timestamp() ) + " sec. on parsing source file.\n" )
+
+
+#start writing to file
+print("output start")
+transformed_path = "D:\\PyWorkspace\\Amazon20030601_transform.txt"
+with open(transformed_path,"w") as transformed_file:
+    for i, edge in enumerate( edge_weight_list ):
+        transformed_file.write( " ".join( map(str,edge) ) + "\n" )
+        
+print("output done.")
